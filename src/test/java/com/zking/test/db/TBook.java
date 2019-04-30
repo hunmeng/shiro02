b0e8fa125ac46fb0826a03e81ee65aef54a1dacc
hunmeng <2377178424@qq.com>
2019-04-30 18:34:19 +0800

    Initial commit

---
+++ src/test/java/com/zking/test/db/TBook.java
@@ -0,0 +1,37 @@
+package com.zking.test.db;
+
+
+public class TBook {
+
+  private long bookId;
+  private String bookName;
+  private double price;
+
+
+  public long getBookId() {
+    return bookId;
+  }
+
+  public void setBookId(long bookId) {
+    this.bookId = bookId;
+  }
+
+
+  public String getBookName() {
+    return bookName;
+  }
+
+  public void setBookName(String bookName) {
+    this.bookName = bookName;
+  }
+
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+}
